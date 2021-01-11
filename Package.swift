a9c49237195268f0606f1d2262f547b74929d593
Sam Pettersson <sampettersson@Sams-Mac-mini.local>
2021-01-11 13:14:30 +0100

    Bump ease

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,11 +8,10 @@ let package = Package(
     products: [
         .library(
             name: "Ease",
-            type: .dynamic,
             targets: ["Ease"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hedviginsurance/flow", .upToNextMajor(from: "1.8.6")),
+        .package(url: "https://github.com/hedviginsurance/flow", .branch("master")),
     ],
     targets: [
         .target(
