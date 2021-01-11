eb78479b7beebdffa3e5d29493e36e8df4661f73
Sam Pettersson <sampettersson@Sams-Mac-mini.local>
2021-01-11 13:20:46 +0100

    Fix flow dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Ease"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hedviginsurance/flow", .branch("master")),
+        .package(url: "https://github.com/hedviginsurance/Flow", .branch("master")),
     ],
     targets: [
         .target(
